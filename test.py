2c5489ca95b562477abd3b2c77d1a2144ae85b09
Abhay Pratap Singh <abhaypratap3536@gmail.com>
2019-02-07 02:17:39 +0530

    updates test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#import greeting 
-import greeting_ask as greeting
+import greeting
+#import greeting_ask as greeting
 import unittest
 import json
 
@@ -9,7 +9,7 @@ class GreetingTestCase(unittest.TestCase):
     def setUp(self):
         greeting.app.config['ASK_VERIFY_REQUESTS'] = False
         self.app = greeting.app.test_client()
-        with open('event.json') as data_file:    
+        with open('event.json') as data_file:
             self.jdata = json.load(data_file)
 
     def tearDown(self):
@@ -34,40 +34,40 @@ class GreetingTestCase(unittest.TestCase):
         self.check_valid_response(response,end_session=True)
         self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'<speak>Hello.*</speak>', 'Output speech text check')
 
-    def test_02_quote_intent(self):
-        jdata = self.jdata.copy()
-        jdata['request']['intent']['name'] = 'QuoteIntent'
-        jdata['request']['intent']['slots'] = {}
-
-        response = self.get_repsonse(jdata)
-        self.check_valid_response(response,end_session=False)
-        GreetingTestCase.prev_response = response
-        self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'Do you.*', 'Output speech text check')
-        self.assertRegexpMatches(response['response']['reprompt']['outputSpeech']['ssml'],r'^<speak>.*You can say.*</speak>', 'reprompt text valid')
-
-    def test_03_next_quote_intent(self):
-        jdata = self.jdata.copy()
-        jdata['request']['intent']['name'] = 'NextQuoteIntent'
-        jdata['request']['intent']['slots'] = {}
-        jdata['session']['attributes'] = GreetingTestCase.prev_response['sessionAttributes']
-
-        response = self.get_repsonse(jdata)
-        self.check_valid_response(response,end_session=False)
-        self.assertRegexpMatches(response['response']['reprompt']['outputSpeech']['ssml'],r'^<speak> You can say.*</speak>', 'reprompt text valid')
-
-        jdata['session']['attributes'] = {}
-        response = self.get_repsonse(jdata)
-        self.check_valid_response(response,end_session=True)
-        self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'^<speak>Wrong.*</speak>', 'Output speech text check')
-
-    def test_04_stop_intent(self):
-        jdata = self.jdata.copy()
-        jdata['request']['intent']['name'] = 'AMAZON.StopIntent'
-        jdata['request']['intent']['slots'] = {}
-
-        response = self.get_repsonse(jdata)
-        self.check_valid_response(response,end_session=True)
-        self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'^<speak>Good Bye.</speak>', 'speech text check')
+    # def test_02_quote_intent(self):
+    #     jdata = self.jdata.copy()
+    #     jdata['request']['intent']['name'] = 'QuoteIntent'
+    #     jdata['request']['intent']['slots'] = {}
+    #
+    #     response = self.get_repsonse(jdata)
+    #     self.check_valid_response(response,end_session=False)
+    #     GreetingTestCase.prev_response = response
+    #     self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'Do you.*', 'Output speech text check')
+    #     self.assertRegexpMatches(response['response']['reprompt']['outputSpeech']['ssml'],r'^<speak>.*You can say.*</speak>', 'reprompt text valid')
+    #
+    # def test_03_next_quote_intent(self):
+    #     jdata = self.jdata.copy()
+    #     jdata['request']['intent']['name'] = 'NextQuoteIntent'
+    #     jdata['request']['intent']['slots'] = {}
+    #     jdata['session']['attributes'] = GreetingTestCase.prev_response['sessionAttributes']
+    #
+    #     response = self.get_repsonse(jdata)
+    #     self.check_valid_response(response,end_session=False)
+    #     self.assertRegexpMatches(response['response']['reprompt']['outputSpeech']['ssml'],r'^<speak> You can say.*</speak>', 'reprompt text valid')
+    #
+    #     jdata['session']['attributes'] = {}
+    #     response = self.get_repsonse(jdata)
+    #     self.check_valid_response(response,end_session=True)
+    #     self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'^<speak>Wrong.*</speak>', 'Output speech text check')
+    #
+    # def test_04_stop_intent(self):
+    #     jdata = self.jdata.copy()
+    #     jdata['request']['intent']['name'] = 'AMAZON.StopIntent'
+    #     jdata['request']['intent']['slots'] = {}
+    #
+    #     response = self.get_repsonse(jdata)
+    #     self.check_valid_response(response,end_session=True)
+    #     self.assertRegexpMatches(response['response']['outputSpeech']['ssml'],r'^<speak>Good Bye.</speak>', 'speech text check')
 
     def check_valid_response(self,response,end_session=True):
         self.assertEqual(response['version'],'1.0',"Version check")
